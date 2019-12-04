b51ee41a57907700c8aca65dedce2a888bfde944
RogerTb1 <55488871+RogerTb1@users.noreply.github.com>
2019-12-04 12:56:28 +0100

    Add files via upload

--- arith.py
+++ arith.py
@@ -1,6 +1,7 @@
 import markovify
 from numpy import cumsum
 from fractions import Fraction
+from collections import defaultdict
 
 text = "Example phrase. This is another example sentence. And another one"
 text_model = markovify.Text(text)
@@ -26,15 +27,10 @@ def find_fraction(input_start, input_end):
         output_fraction = Fraction(output_numerator, output_denominator)
         output_denominator *= 2
     
-
     return output_fraction
 
 
     
-#Suposem donat una taula de frequencies (FALTA FER)
-freqTab = [1]*256
-
-
 def get_next_character(f):
     """Reads one character from the given textfile"""
     c = f.read(1)
@@ -42,45 +38,84 @@ def get_next_character(f):
         yield c
         c = f.read(1)
         
-        
-with open("corpus.txt") as f:
-    count = 0
-    for c in get_next_character(f):
-        count += 1
-        charASCII = ord(c)
-        freqTab[charASCII] += 1
+    
 
-
-#S'ha de crear una taula de frequencies acumulades
-sumFreq = cumsum(freqTab)
-cumFreq = [0]*257
-for i in range(1, 257):
-    cumFreq[i] = float(sumFreq[i - 1]) / float(sumFreq[-1])
-
-
-#Suposem que tenim un input 
-entrada = "havia una vez un cerdito , muy bonito que viajaba todo el dia por  el valle y un dia se encontro "
-interval_inf = 0
-interval_sup = 1
-length = len(entrada)
-
-for i in range(length):
-    lengthInterval = interval_sup - interval_inf
-    char = entrada[i]
-    charASCII = ord(char)
-    interval_inf = cumFreq[charASCII] * lengthInterval
-    interval_sup = cumFreq[charASCII + 1] * lengthInterval
-
-print(interval_inf)
-print(interval_sup)
+def calculate_cum_freq(freqTab):
+    fqa =cumsum(freqTab.values())
+    length = len(freqTab) + 1
+    cumFreq = [0] * length
+    for i in range(1, length):
+        cumFreq[i] = float(fqa[i - 1]) / float(fqa[-1])
+    cFreq = (freqTab.keys(), cumFreq)
+    return cFreq
     
 
 
 
+    
+#Suposem que tenim un input 
+entrada = "maravilloso"
+interval_inf = 0
+interval_sup = 1
+length = len(entrada)
+freqTab = defaultdict(int)
 
+fixed = True
+
+for i in range(ord('a'), ord('z')):
+    freqTab[chr(i)] = 1;
+    
+for i in range(ord('0'), ord('9')):
+    freqTab[chr(i)] = 1;
+    
+    
+if (fixed):
+    with open("corpus.txt") as f:
+        for c in get_next_character(f):
+            string = str(c)
+            if(string.isupper()):
+                string = string.lower()
+                freqTab[string] += 1
+            elif (c in freqTab):
+                freqTab[c] += 1
+    for k, f in freqTab.items():
+        if f < 500:
+            del freqTab[k]
+    keys,cumFreq = calculate_cum_freq(freqTab)   
+    
+    for i in range(length):
+        lengthInterval = interval_sup - interval_inf
+        char = entrada[i]
+        j = keys.index(char)
+      
+
+        interval_inf = cumFreq[j] * lengthInterval
+        interval_sup = cumFreq[j+ 1] * lengthInterval
+        
+       
+
+else: 
+    for i in range(length):
+        lengthInterval = interval_sup - interval_inf
+        keys, cumFreq = calculate_cum_freq(freqTab)
+        char = entrada[i]
+        j = keys.index(char)
+
+        interval_inf = cumFreq[j] * lengthInterval
+        interval_sup = cumFreq[j + 1] * lengthInterval
+        
+
+     
+print(interval_inf)
+print(interval_sup)
+print((interval_inf + interval_sup) / 2.)
+    
+    
 valor_entrada = find_fraction(interval_inf, interval_sup)
 
 print(valor_entrada)
+
+
 
 chain.walk((BEGIN, 'This'))
 # Gives: ['is', 'another', 'example', 'sentence.']
